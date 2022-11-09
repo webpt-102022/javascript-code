e82b47ca404dc512e0df3fcabb9b075542f2e7ef
Ale Bausa <ale.bausa@gmail.com>
2022-11-09 14:18:08 +0100

    adds function topic

--- functions.js
+++ functions.js
@@ -1,5 +1,5 @@
 
-const ta = 'Marina';
+const ta = 'Marina'; // Variable shadowing
 
 if (true) {
   const ta = 'Guillem';
@@ -16,8 +16,7 @@ function myFunction(num1, num2) { // var
 
 myFunction(3, 4, 5, 6)
 
-// Function expression
-
+// Function expression = función anónima almacenada en una variable
 let mySecondFunction = function (num1, num2) {
   return num1 + num2;
 }
@@ -25,17 +24,13 @@ let mySecondFunction = function (num1, num2) {
 mySecondFunction()
 
 // Arrow function
-
-const sumFunction = (num1, num2) => {
+const sumFunction = (num1, num2) => { // Evolución del function expression
   const result = (num1 + num2)
   return result;
 }
 
-sumFunction(45, 56);
-sumFunction(65, 56);
 
 // Callback functions
-
 const step1 = () => {
   console.log('Make the dough')
 }
@@ -59,7 +54,7 @@ const makePizza = (func1, func2, func3, func4) => {
   func4();
 }
 
-makePizza(step1, step2, step3, () => console.log('Put in oven')); // Callback function
+makePizza(step1, step2, step3, () => console.log('Put in oven')); // Callback function: la puedo crear aquí mismo
 
 const numbers = [1, 2, 3];
 
@@ -67,8 +62,9 @@ const add2 = (elem, i, arr) => {
   arr[i] = elem + 2;
 }
 
-numbers.forEach(add2)
-console.log(numbers)
+numbers.forEach(add2) // Con callback function creada fuera
+numbers.forEach((num, i, arr) => arr[i] = num + 2) // Con callback function creada aquí mismo (en este caso una arrow function anónima)
+
 
 
 
